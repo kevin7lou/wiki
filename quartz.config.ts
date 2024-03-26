7090acaaeb170dc40da1672d125ec4ebda337060
kevin7lou <1493360+kevin7lou@users.noreply.github.com>
2024-03-26 10:42:01 +0800

    Quartz sync: Mar 26, 2024, 10:42 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🌱 KevinL's Wiki",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
